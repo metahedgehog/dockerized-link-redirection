bf73d46df512748b90e2446f1c2455a1f4dd0491
metahedgehog <ostapchenko.danil@gmail.com>
2023-10-16 18:01:38 +0300

    Update test_app.py

--- web/tests/test_app.py
+++ web/tests/test_app.py
@@ -12,17 +12,6 @@ class TestApp(unittest.TestCase):
         response = self.app.get('/')
         self.assertEqual(response.status_code, 302)  # Check if the response is a redirection
 
-    def test_count_file_missing(self):
-        # Rename the count.txt file to simulate its absence
-        os.rename('/app/data/count.txt', '/app/data/count.txt.bak')
-        response = self.app.get('/counter', data={'data': '/app/data'})
-        self.assertEqual(response.status_code, 200)
-        self.assertIn(b'Visits: 0', response.data)  # Check if the count is 0
-
-        # Restore the count.txt file if it was renamed
-        if os.path.exists('/app/data/count.txt.bak'):
-            os.rename('/app/data/count.txt.bak', '/app/data/count.txt')
-
     def tearDown(self):
         pass
 
